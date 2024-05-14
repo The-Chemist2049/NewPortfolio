654ea8863bc17212dc4c603fd0d74c4e1ec35d05
Bonface Makokha <90345615+The-Chemist2049@users.noreply.github.com>
2024-05-14 17:48:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["a Realtor", "a Connector", "a Curator", "a Director", "an Administrator"],
+        strings: [" a Realtor", " a Connector", " a Curator", " a Director", " an Administrator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
